05aff913da68f272fb83932959e2bacdc8e5636f
Jaesung-decoder <152773837+Jaesung-decoder@users.noreply.github.com>
2023-12-03 20:22:09 +0900

    Create W1_Practice03.java
    Discount calculation
    1. base entry fee is 10,000.
    2. if age < 3: free entry
    3. if age < 13: discount applied and fee is 4,000
    4. if enters after 17:00: discount applied and fee is 4,000
    5. if the person is a national merit holder or welfare card holder, discount applied and fee is 8,000

---
+++ W1_Practice03.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class W1_Practice03 {
+    public static void main(String[] args){
+        System.out.println("[입장권 계산]");
+        System.out.print("나이를 입력해 주세요. (숫자):");
+        Scanner typeAge = new Scanner(System.in);
+        int age = typeAge.nextInt();
+
+        System.out.print("입장시간을 입력해 주세요. (숫자입력):");
+        Scanner enterHour = new Scanner(System.in);
+        int hour = enterHour.nextInt();
+
+        System.out.print("국가유공자 여부를 입력해 주세요.(y/n):");
+        Scanner flag = new Scanner(System.in);
+        String merit = flag.next();
+
+        System.out.print("복지카드 여부를 입력해 주세요.(y/n):");
+        Scanner card = new Scanner(System.in);
+        String welfareCard = card.next();
+
+        int entryFare = 10000;
+        if(age < 3){
+            entryFare = 0;
+        } else if (age < 13) {
+            entryFare = 4000;
+        } else if (hour >= 17) {
+            entryFare = 4000;
+        } else if (merit.equals("y") || welfareCard.equals("y")) {
+            entryFare = 8000;
+        }
+        System.out.println("입장료: " + entryFare);
+    }
+}
