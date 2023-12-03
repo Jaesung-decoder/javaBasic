f7716a091d4e400e4913bf180f9ef5812f503eb0
Jaesung-decoder <152773837+Jaesung-decoder@users.noreply.github.com>
2023-12-03 19:50:44 +0900

    Create Practice02.java
    1. Maximum cashback amount is 300. 
    2. cashback is calculated by 100, available cashback amount is 100, 200 and 300.
    3. 10% of total paid amount shall be saved as cashback.

---
+++ Practice02.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Practice02 {
+  public static void main(String[] Args){
+    System.out.println("[캐시백 계산]");
+    System.out.print("결제 금액을 입력해 주세요. (금액):");
+    Scanner in = new Scanner(System.in);
+    int totalCharge = in.nextInt();
+
+    int cashBack = 0;
+    if ((totalCharge * 0.1) / 100 > 3){
+        cashBack = 3*100;
+      }else{
+            cashBack =(int)((totalCharge * 0.1)/100)*100;
+        }
+        System.out.println("결제 금액은 " + totalCharge + "원이고, 캐시백은 " + cashBack + ("원 입니다."));
+
+    }
+}
