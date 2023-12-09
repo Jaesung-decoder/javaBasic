b1d924a244ab4001c054238b70312bbe8fd3da54
Jaesung-decoder <152773837+Jaesung-decoder@users.noreply.github.com>
2023-12-09 16:45:33 +0900

    Update W1_Practice01.java

--- W1_Practice01.java
+++ W1_Practice01.java
@@ -4,7 +4,7 @@ public class W1_Practice01 {
           for(int i=1; i<10; i++){
               for(int j=1; j<10; j++){
                   int k = i*j;
-                  System.out.print(String.format("%02d", i) + " x " + String.format("%02d", j) + " = " +String.format("%02d", k));
+                  System.out.print(String.format("%02d", j) + " x " + String.format("%02d", i) + " = " +String.format("%02d", k));
                   System.out.print("    ");
               }
               System.out.println();
